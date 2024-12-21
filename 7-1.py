70d147f40d9ce2fcd64bc2694953f20ad0b65414
sevilledev <sevillerasulova@gmail.com>
2024-12-21 15:16:46 +0400

    Added Day 7 solution

---
+++ 7-1.py
@@ -0,0 +1,29 @@
+equations = [list(e.split(": ")) for e in open("puzzle.txt").read().splitlines()]
+
+total = 0
+
+
+def check(operands, result):
+    def calc(new_index, res):
+        if new_index == len(operands):
+            return res == result
+
+        next_operand = operands[new_index]
+        
+        addition = calc(new_index + 1, res + next_operand)
+        multiply = calc(new_index + 1, res * next_operand)
+
+        return addition or multiply
+
+    return calc(1, operands[0])
+
+
+for equation in equations:
+    result = int(equation[0])
+    operands = list(int(n) for n in equation[1].split())
+    
+    if check(operands, result):
+        total += result
+        
+
+print(total)
